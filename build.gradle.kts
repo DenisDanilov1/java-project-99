54a1f38a41d3a18624909fa15c25325b0072379b
DenisDanilov1 <denchik199819981998@gmail.com>
2024-09-23 14:20:45 +0400

    added the util folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,84 @@
 plugins {
-    id("java")
+    application
+    checkstyle
+    jacoco
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("io.freefair.lombok") version "8.6"
+    id("io.sentry.jvm.gradle") version "4.11.0"
 }
 
-group = "org.example"
+group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.AppApplication") }
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    runtimeOnly("com.h2database:h2")
+    implementation("org.postgresql:postgresql:42.7.3")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-api:2.6.0")
+
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.3.0")
+
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+}
+checkstyle {
+    toolVersion = "7.6.1"
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
+
+sentry {
+    debug = true
+
+    includeSourceContext = true
+
+    org = "nodweb"
+    projectName = "java-spring-boot"
+    authToken = System.getenv("SENTRY_AUTH_TOKEN")
+}
+
+tasks.sentryBundleSourcesJava {
+    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
 }
