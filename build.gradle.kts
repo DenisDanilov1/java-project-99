503db217ca94741db7c7902a0528c7f8d57fea6f
DenisDanilov1 <denchik199819981998@gmail.com>
2024-09-23 14:24:41 +0400

    made some changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
+
 checkstyle {
     toolVersion = "7.6.1"
 }
