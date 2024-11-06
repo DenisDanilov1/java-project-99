3122793c2b9b0d9e9753577ecf292a45d1cf8f8d
DenisDanilov1 <denchik199819981998@gmail.com>
2024-11-06 22:17:51 +0400

    made some changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     id("io.freefair.lombok") version "8.6"
-    id("io.sentry.jvm.gradle") version "4.11.0"
+    id("io.sentry.jvm.gradle") version "4.13.0"
 }
 
 group = "hexlet.code"
