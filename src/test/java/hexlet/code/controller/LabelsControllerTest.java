4b94ea798afb3e3e9a7bdd2bae6c804420cfa8c3
DenisDanilov1 <denchik199819981998@gmail.com>
2024-11-26 16:42:13 +0400

    changed a list of entities request

--- src/test/java/hexlet/code/controller/LabelsControllerTest.java
+++ src/test/java/hexlet/code/controller/LabelsControllerTest.java
@@ -2,16 +2,18 @@ package hexlet.code.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.controller.util.ModelGenerator;
 import hexlet.code.dto.label.LabelCreateDTO;
 import hexlet.code.dto.label.LabelDTO;
 import hexlet.code.dto.label.LabelUpdateDTO;
 import hexlet.code.mapper.LabelMapper;
 import hexlet.code.model.Label;
 import hexlet.code.repository.LabelRepository;
-import hexlet.code.controller.util.ModelGenerator;
+
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
+
 import org.assertj.core.api.Assertions;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.AfterEach;
@@ -45,24 +47,25 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class LabelControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+class LabelsTest {
 
     @Autowired
     private WebApplicationContext wac;
 
     @Autowired
-    private TaskRepository taskRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    private MockMvc mockMvc;
 
     @Autowired
     private LabelRepository labelRepository;
 
     @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
     private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private ModelGenerator modelGenerator;
@@ -70,21 +73,22 @@ class LabelControllerTest {
     @Autowired
     private ObjectMapper om;
 
+    @Autowired
     private LabelMapper labelMapper;
 
     private Label testLabel;
 
     private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
 
-
     @BeforeEach
     public void setUp() {
+
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
                 .apply(springSecurity())
                 .build();
 
-        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
+        token = jwt().jwt(builder -> builder.subject("test@test.ru"));
         testLabel = Instancio.of(modelGenerator.getLabelModel()).create();
 
         labelRepository.save(testLabel);
@@ -101,21 +105,17 @@ class LabelControllerTest {
 
     @Test
     public void testGetAll() throws Exception {
-        var response = mockMvc.perform(get("/api/labels").with(token))
+        var response = mockMvc.perform(get("/api/labels").with(jwt()))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse();
-
         var body = response.getContentAsString();
 
-        List<LabelDTO> labelDTOS = om.readValue(body, new TypeReference<>() { });
+        List<LabelDTO> labelDTO = om.readValue(body, new TypeReference<>() { });
 
-        var actual = labelDTOS.stream()
-                .map(labelMapper::map)
-                .toList();
-        var excepted = labelRepository.findAll();
-
-        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(excepted);
+        List<Label> actual = labelDTO.stream().map(labelMapper::map).toList();
+        List<Label> expected = labelRepository.findAll();
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
     }
 
     @Test
@@ -127,7 +127,7 @@ class LabelControllerTest {
     @Test
     public void testCreate() throws Exception {
         var data = new LabelCreateDTO();
-        data.setName("testName");
+        data.setName("test");
 
         var request = post("/api/labels")
                 .with(token)
@@ -136,7 +136,8 @@ class LabelControllerTest {
 
         mockMvc.perform(request).andExpect(status().isCreated());
 
-        var label = labelRepository.findByName(data.getName()).get();
+        var label = labelRepository.findByName(data.getName())
+                .orElseThrow(() -> new IllegalArgumentException("Label not found!"));
 
         assertNotNull(label);
         assertThat(label.getName()).isEqualTo(data.getName());
@@ -144,18 +145,21 @@ class LabelControllerTest {
 
     @Test
     public void testUpdate() throws Exception {
-        var data = new LabelUpdateDTO();
-        data.setName(JsonNullable.of("newName"));
+        var updatedData = new LabelUpdateDTO();
+        updatedData.setName(JsonNullable.of("new"));
+
         var request = put("/api/labels/" + testLabel.getId())
                 .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(om.writeValueAsString(data));
+                .content(om.writeValueAsString(updatedData));
 
         mockMvc.perform(request).andExpect(status().isOk());
 
-        var updatedLabel = labelRepository.findByName(data.getName().get()).get();
+        var updatedLabel = labelRepository.findById(testLabel.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Label not found!"));
 
         assertNotNull(updatedLabel);
+        assertThat(updatedLabel.getName()).isEqualTo(updatedData.getName().get());
     }
 
     @Test
